6c8ce8c4e1bb9cf485a76ec23a8dc84745e730df
Marcio M. Ribeiro <marcio.mr@gmail.com>
2017-03-12 20:01:00 -0300

    Refatoração: atr_reader

--- app/models/password.rb
+++ app/models/password.rb
@@ -1,12 +1,10 @@
 class Password
+  attr_reader :array, :dictionary
+  
   def initialize(size)
     @dictionary = YAML.load_file("#{Rails.root}/config/dicionario.yml")
     @array = size.times.map do
       @dictionary.sample
     end
-  end
-
-  def array
-    @array
   end
 end
