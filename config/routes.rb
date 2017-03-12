8533d6e48e3dfdd8feea893420addb34d0ef6f05
Marcio M. Ribeiro <marcio.mr@gmail.com>
2017-03-12 20:31:47 -0300

    Issue #2 resolvido (closed)

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   get 'password/new' => 'password#new'
+  get 'password/:size' => 'password#show'
 end
