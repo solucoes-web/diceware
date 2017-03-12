b62cf8c1ba4ee79a784c007535f95cb2d725d6b0
Marcio M. Ribeiro <marcio.mr@gmail.com>
2017-03-12 20:07:55 -0300

    Issue #1 resolvida (closed)

--- app/models/password.rb
+++ app/models/password.rb
@@ -4,7 +4,8 @@ class Password
   def initialize(size)
     @dictionary = YAML.load_file("#{Rails.root}/config/dicionario.yml")
     @array = size.times.map do
-      @dictionary.sample
+       index = Sysrandom.random_number(@dictionary.size)
+       @dictionary[index]
     end
   end
 end
