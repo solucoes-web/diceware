d27cf13a9ea041207c107858395a9f04d1ac829b
Marcio M. Ribeiro <marcio.mr@gmail.com>
2017-03-12 21:21:38 -0300

    Refatoração: movi a entropia para o modelo

--- app/helpers/password_helper.rb
+++ app/helpers/password_helper.rb
@@ -1,8 +1,4 @@
 module PasswordHelper
-  def entropy(number_of_words)
-    Math::log 6**(5*number_of_words), 2
-  end
-  
   def paridade(i)
     i%2 == 0 ? 'par' : 'impar'
   end
--- app/models/password.rb
+++ app/models/password.rb
@@ -1,11 +1,16 @@
 class Password
-  attr_reader :array, :dictionary
+  attr_reader :array, :dictionary, :size
   
   def initialize(size)
+    @size = size
     @dictionary = YAML.load_file("#{Rails.root}/config/dicionario.yml")
-    @array = size.times.map do
+    @array = @size.times.map do
        index = Sysrandom.random_number(@dictionary.size)
        @dictionary[index]
     end
   end
+
+  def entropy
+    Math::log @dictionary.size**@size, 2
+  end  
 end
